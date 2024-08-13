1b1fe3492e127d4d064a1851ecd28e8b295096b7
rajan-deeep <134048013+rajan-deeep@users.noreply.github.com>
2024-08-14 00:03:30 +0530

    redis config

---
+++ src/main/java/org/panda/config/RedisConfig.java
@@ -0,0 +1,46 @@
+package org.panda.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.listener.RedisMessageListenerContainer;
+
+@Configuration
+@Slf4j
+public class RedisConfig {
+
+    @Value("${SPRING_REDIS_HOST:localhost}")
+    private String redisHost;
+
+    @Value("${SPRING_REDIS_PORT:6379}")
+    private int redisPort;
+
+    @Bean
+    public LettuceConnectionFactory lettuceConnectionFactory() {
+        RedisStandaloneConfiguration configuration = new RedisStandaloneConfiguration(redisHost, redisPort);
+        LettuceConnectionFactory lettuceConnectionFactory = new LettuceConnectionFactory(configuration);
+
+        // Log Redis connection info
+        log.info("Connecting to Redis server at {}:{}", configuration.getHostName(), configuration.getPort());
+        return lettuceConnectionFactory;
+    }
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate() {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(lettuceConnectionFactory());
+        return template;
+    }
+
+    @Bean
+    RedisMessageListenerContainer redisMessageListenerContainer(RedisConnectionFactory redisConnectionFactory) {
+        RedisMessageListenerContainer container = new RedisMessageListenerContainer();
+        container.setConnectionFactory(redisConnectionFactory);
+        return container;
+    }
+}
